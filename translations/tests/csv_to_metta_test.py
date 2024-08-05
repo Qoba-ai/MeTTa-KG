2420930019192ddff42c6f4d11081c42394c2605
AnnelineD <anneline.daggelinckx@hotmail.com>
2024-08-05 19:29:23 +0200

    Row based MeTTa to CSV translation

--- translations/tests/csv_to_metta_test.py
+++ translations/tests/csv_to_metta_test.py
@@ -38,10 +38,11 @@ class CSVToMetta(unittest.TestCase):
                          matrix_to_header_row_based(self.m))
 
     def test_struct_based(self):
-        self.assertEqual('(("Index" "1") ("Name" "Alice Johnson") ("Phone" "384.555.0192x123") ("Website" "http://www.alicejservices.com/"))\n'
-                         '(("Index" "2") ("Name" "Michael Smith") ("Phone" "(512)987-6543x56789") ("Website" "http://www.msmithtech.net/"))\n'
-                         '(("Index" "3") ("Name" "Emily Davis") ("Phone" "+1-310-555-6789") ("Website" "http://www.emilydavisconsulting.org/"))',
-                         dict_to_struct_based_metta(self.d))
+        self.assertEqual(
+            '(("Index" "1") ("Name" "Alice Johnson") ("Phone" "384.555.0192x123") ("Website" "http://www.alicejservices.com/"))\n'
+            '(("Index" "2") ("Name" "Michael Smith") ("Phone" "(512)987-6543x56789") ("Website" "http://www.msmithtech.net/"))\n'
+            '(("Index" "3") ("Name" "Emily Davis") ("Phone" "+1-310-555-6789") ("Website" "http://www.emilydavisconsulting.org/"))',
+            dict_to_struct_based_metta(self.d))
 
     def test_field_based(self):
         self.assertEqual('(0 "Index" "1")\n'
@@ -57,6 +58,43 @@ class CSVToMetta(unittest.TestCase):
                          '(2 "Phone" "+1-310-555-6789")\n'
                          '(2 "Website" "http://www.emilydavisconsulting.org/")',
                          dict_to_field_based_metta(self.d))
+
+
+class MeTTaToCSV(unittest.TestCase):
+    def test_matrix_to_csv(self):
+        s1 = ("Index,Name,Phone,Website\n"
+             "1,Alice Johnson,384.555.0192x123,http://www.alicejservices.com/\n"
+             "2,Michael Smith,(512)987-6543x56789,http://www.msmithtech.net/\n"
+             "3,Emily Davis,+1-310-555-6789,http://www.emilydavisconsulting.org/\n")
+        s2 = matrix_to_csv_str([["Index", "Name", "Phone", "Website"],
+                                            ["1", "Alice Johnson", "384.555.0192x123",
+                                             "http://www.alicejservices.com/"],
+                                            ["2", "Michael Smith", "(512)987-6543x56789", "http://www.msmithtech.net/"],
+                                            ["3", "Emily Davis", "+1-310-555-6789",
+                                             "http://www.emilydavisconsulting.org/"]])
+
+        self.assertEqual("Index,Name,Phone,Website\r\n"
+                         "1,Alice Johnson,384.555.0192x123,http://www.alicejservices.com/\r\n"
+                         "2,Michael Smith,(512)987-6543x56789,http://www.msmithtech.net/\r\n"
+                         "3,Emily Davis,+1-310-555-6789,http://www.emilydavisconsulting.org/\r\n",
+                         matrix_to_csv_str([["Index", "Name", "Phone", "Website"],
+                                            ["1", "Alice Johnson", "384.555.0192x123",
+                                             "http://www.alicejservices.com/"],
+                                            ["2", "Michael Smith", "(512)987-6543x56789", "http://www.msmithtech.net/"],
+                                            ["3", "Emily Davis", "+1-310-555-6789",
+                                             "http://www.emilydavisconsulting.org/"]])
+                         )
+
+    def test_row_based_to_matrix(self):
+        self.assertEqual(["test"], ['test'])
+        self.assertEqual([["Index", "Name", "Phone", "Website"],
+                          ["1", "Alice Johnson", "384.555.0192x123", "http://www.alicejservices.com/"],
+                          ["2", "Michael Smith", "(512)987-6543x56789", "http://www.msmithtech.net/"],
+                          ["3", "Emily Davis", "+1-310-555-6789", "http://www.emilydavisconsulting.org/"]],
+                         matrix_from_row_based_metta('(0 ("Index" "Name" "Phone" "Website"))\n'
+                                                     '(1 ("1" "Alice Johnson" "384.555.0192x123" "http://www.alicejservices.com/"))\n'
+                                                     '(2 ("2" "Michael Smith" "(512)987-6543x56789" "http://www.msmithtech.net/"))\n'
+                                                     '(3 ("3" "Emily Davis" "+1-310-555-6789" "http://www.emilydavisconsulting.org/"))'))
 
 
 if __name__ == '__main__':
