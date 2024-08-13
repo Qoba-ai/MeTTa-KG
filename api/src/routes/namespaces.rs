a08f2b1c3e0bfcd87486da75146f712b394b1d81
tim verhaegen <tim.verhaegen@hotmail.be>
2024-08-13 16:59:16 +0200

    backend init

---
+++ api/src/routes/namespaces.rs
@@ -0,0 +1,39 @@
+use diesel::{ExpressionMethods, QueryDsl, RunQueryDsl, SelectableHelper};
+use rocket::http::Status;
+use rocket::serde::json::Json;
+use rocket::{get, post};
+
+use super::TokenClaims;
+
+use crate::{db::establish_connection, model::Namespace};
+
+#[get("/namespaces")]
+pub fn get_all() -> (Status, Option<Json<Vec<Namespace>>>) {
+    use crate::schema::namespaces::dsl::*;
+
+    let conn = &mut establish_connection();
+
+    let results = namespaces.select(Namespace::as_select()).get_results(conn);
+
+    match results {
+        Ok(results) => (Status::Ok, Some(Json(results))),
+        Err(_) => (Status::InternalServerError, None),
+    }
+}
+
+#[post("/namespaces", data = "<namespace>")]
+pub fn create(claims: TokenClaims, namespace: Json<Namespace>) -> Status {
+    use crate::schema::namespaces::dsl::*;
+
+    let conn = &mut establish_connection();
+
+    // TODO: replace execute by get_result when switching to postgres
+    let result = diesel::insert_into(namespaces)
+        .values((name.eq(&namespace.name), user_id.eq(claims.user_id)))
+        .execute(conn);
+
+    match result {
+        Ok(_) => Status::Ok,
+        Err(_) => Status::InternalServerError,
+    }
+}
