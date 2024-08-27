8f59161b7ee31ce302cc8887865e564daa1cba6e
tim verhaegen <tim.verhaegen@hotmail.be>
2024-08-27 03:58:11 +0200

    sample translation UI

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,4 +1,5 @@
 use rocket::{self, launch, routes, Build, Rocket};
+use rocket_cors::{AllowedHeaders, AllowedOrigins};
 
 mod db;
 mod model;
@@ -7,13 +8,24 @@ mod schema;
 
 #[launch]
 fn rocket() -> Rocket<Build> {
-    rocket::build().mount(
-        "/",
-        routes![
-            routes::namespaces::get_all,
-            routes::namespaces::create,
-            routes::sessions::create,
-            routes::translations::create
-        ],
-    )
+    let allowed_origins = AllowedOrigins::some_exact(&["http://localhost:3000"]);
+
+    let cors = rocket_cors::CorsOptions {
+        allowed_origins,
+        ..Default::default()
+    }
+    .to_cors()
+    .unwrap();
+
+    rocket::build()
+        .mount(
+            "/",
+            routes![
+                routes::namespaces::get_all,
+                routes::namespaces::create,
+                routes::sessions::create,
+                routes::translations::create
+            ],
+        )
+        .attach(cors)
 }
