e90ea85e0fd9c22abfb02afee3854dc473f058dc
surafel_fikru <surafelfikru76@gmail.com>
2025-08-25 14:03:59 +0300

    refactor header UI and change theme

---
+++ frontend/src/components/common/header.tsx
@@ -0,0 +1,32 @@
+import { Sun, Moon } from "lucide-solid"
+import NameSpace from "./nameSpace"
+import { Accessor, createSignal } from "solid-js"
+import { Button } from "../ui/button"
+import { useColorMode } from "@kobalte/core"
+
+
+interface HeaderProps {
+    activeTab: Accessor<string>,
+    setActiveTab: (tab: string) => void
+}
+
+export default function Header() {
+    const { colorMode, setColorMode } = useColorMode()
+
+    return (
+        <>
+            {/* Header */}
+            <header class="bg-card border-b border-border p-4">
+                <div class="flex items-center justify-between px-4">
+                    <div class="flex items-center gap-4">
+                        <NameSpace />
+                        {/* Theme Toggle */}
+                    </div>
+                    <Button variant="ghost" size="sm" onClick={() => setColorMode(colorMode() === "dark" ? "light" : "dark")}>
+                        {colorMode() === "dark" ? <Sun class="h-4 w-4" /> : <Moon class="h-4 w-4" />}
+                    </Button>
+                </div>
+            </header>
+        </>
+    )
+}
