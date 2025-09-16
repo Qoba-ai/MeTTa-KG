493d57d593f904a1af0e0ca95f605972ae52da0f
DagmawiKK <dagmawikassa1@gmail.com>
2025-09-16 10:43:56 +0300

    chore: add clear to sidebar

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -14,6 +14,8 @@ import RotateCcw from "lucide-solid/icons/rotate-ccw"
 import Download from "lucide-solid/icons/download"
 import Key from "lucide-solid/icons/key"
 import NotImplemented from "~/components/common/NotImplemented";
+import ClearPage from "./Clear";
+import Trash2 from "lucide-solid/icons/trash-2";
 import { C } from "node_modules/@kobalte/core/dist/collapsible-trigger-6358fcd4";
 
 
@@ -27,7 +29,9 @@ const sidebarSections = [
 			description: "",
 			to: "/",
 			component: LoadPage
-		}],
+		},
+		{ id: "clear", label: "Clear", icon: Trash2, to: "/clear", component: ClearPage },
+	],
 	},
 	{
 		title: "Set and Algebraic Operations",
