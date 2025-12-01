a57df355d73be71450deff199094ad058222958b
DagmawiKK <dagmawikassa1@gmail.com>
2025-12-01 13:12:24 +0300

    fix: expand to fill viewport on trasforma and other commands

--- frontend/src/pages/load/lib.ts
+++ frontend/src/pages/load/lib.ts
@@ -26,34 +26,40 @@ export const [isIndented, setIsIndented] = createSignal(false);
 
 export const handleToggleIndent = () => setIsIndented((p) => !p);
 
-export const [subSpace, { refetch: refetchSubSpace }] = createResource(
-  () => ({
-    path: formatedNamespace(),
-    expr: pattern(),
-    token: Uint8Array.from([]),
-  }),
-  async ({ path, expr, token }) => {
-    try {
-      const data = JSON.parse(
-        await exploreSpace(path, expr, token)
-      ) as ExploreResponse[];
-      showToast({
-        title: "Success",
-        description: `Loaded ${data.length} nodes.`,
-      });
-      return data;
-    } catch (e) {
-      const msg =
-        e instanceof Error && e.message === "noRootToken"
-          ? "No token found, please add one in the Tokens page"
-          : "Failed to load space data.";
-      showToast({ title: "Error", description: msg, variant: "destructive" });
-      return;
+export const [subSpace, { refetch: refetchSubSpace, mutate: mutateSubSpace }] =
+  createResource(
+    () => ({
+      path: formatedNamespace(),
+      expr: pattern(),
+      token: Uint8Array.from([]),
+    }),
+    async ({ path, expr, token }) => {
+      try {
+        const data = JSON.parse(
+          await exploreSpace(path, expr, token)
+        ) as ExploreResponse[];
+        showToast({
+          title: "Success",
+          description: `Loaded ${data.length} nodes.`,
+        });
+        return data;
+      } catch (e) {
+        const msg =
+          e instanceof Error && e.message === "noRootToken"
+            ? "No token found, please add one in the Tokens page"
+            : "Failed to load space data.";
+        showToast({ title: "Error", description: msg, variant: "destructive" });
+        return [];
+      }
     }
-  }
-);
+  );
 
-export const refreshSpace = () => refetchSubSpace();
+export const refreshSpace = () => {
+  mutateSubSpace([]);
+  treeStore.reset();
+  return refetchSubSpace();
+};
+
 export const handleTextChange = (text: string) => setMettaText(text);
 
 export const handlePatternLoad = (newPattern: string) => {
